6d4b1c71eb5318228036ecbf0366eb84c2ea2faa
Richard Race <race.richard@gmail.com>
2013-05-16 18:44:27 +0100

    Added tests for Moving a Rover and Current Position

--- spec/rover_spec.rb
+++ spec/rover_spec.rb
@@ -4,13 +4,35 @@ require './lib/rover'
 require './lib/states/facing_north_state'
 
 describe Rover do
+
+  before(:each) do
+    @grid = Grid.new(10,10)
+  end
+
 	describe "#initialize" do 
 		it "can initialize with x,y, Grid and State params" do
-			grid = Grid.new(10,10)
-			rover = Rover.new(1, 2, grid, FacingNorthState.new)
+			rover = Rover.new(1, 2, @grid, FacingNorthState.new)
 			rover.location[:x].should == 1
 			rover.location[:y].should == 2
 			rover.state.class.should == FacingNorthState
 		end
 	end
+
+  describe "#move" do
+    it "will parse a string in correct format and move to the correct position" do
+      rover = Rover.new(1, 2, @grid, FacingNorthState.new)
+      rover.move("LMLMLMLMM")
+      rover.location[:x].should eq 1
+      rover.location[:y].should eq 3
+      rover.state.direction.should eq "North"
+    end
+  end
+
+  describe "#current_position" do
+    it "will print the correct location in a string" do
+      rover = Rover.new(1, 2, @grid, FacingNorthState.new)
+
+      rover.current_position.should eq "X: 1 Y: 2 Facing: North"
+    end
+  end
 end
